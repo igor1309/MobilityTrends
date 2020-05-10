1b7cbe064578e1db6b0b204985028882fa7b15fe
Igor Malyarov <igor1309@gmail.com>
2020-05-11 01:38:49 +0300

    SimpleChartView with Tide

--- MobilityTrends/One Chart View/SimpleChartView.swift
+++ MobilityTrends/One Chart View/SimpleChartView.swift
@@ -13,67 +13,43 @@ struct SimpleChartView: View {
     @EnvironmentObject var store: Store
     @EnvironmentObject var territories: Territories
     
-    @State private var showSearch = false
-    @State private var showFavorites = false
-    
     var body: some View {
-        NavigationView {
-            VStack {
-                TransportTypePicker(selection: $store.transportType)
-                
-                if store.trend.isNotEmpty {
-                    OneLineChartView(
-                        original: store.trend.series(for: store.selectedRegion, with: store.transportType),
-                        movingAverage: store.trend.movingAverageSeries(for: store.selectedRegion, with: store.transportType),
-                        baseline: store.baseline,
-                        minY: store.trend.selectedRegionMinY(for: store.selectedRegion),
-                        maxY: store.trend.selectedRegionMaxY(for: store.selectedRegion))
+        VStack {
+            CountryTrendsHeader()
+            
+            TransportTypePicker(selection: $store.transportType)
+            
+            if store.tide.trendsForSelected[store.transportType] != nil {
+                OneLineChartView(
+                    original: store.tide.trendsForSelected[store.transportType]!,
+                    movingAverage: store.tide.movingAverageForSelected[store.transportType]!,
+                    baseline: store.baseline,
+                    minY: store.tide.selectedRegionMinY,
+                    maxY: store.tide.selectedRegionMaxY
+                )
+                    .padding(.top)
+            } else {
+                VStack {
+                    Text("No data for \(store.transportType.rawValue) in \(store.selectedRegion)")
                         .padding(.top)
-                } else {
-                    VStack {
-                        Text("No data for \(store.transportType.rawValue) in \(store.selectedRegion)")
-                            .padding(.top)
-                            .foregroundColor(.systemRed)
-                            .opacity(0.6)
-                        Spacer()
-                    }
+                        .foregroundColor(.systemRed)
+                        .opacity(0.6)
+                    Spacer()
                 }
             }
-            .padding()
-            .navigationBarTitle(Text("Mobility Trends"), displayMode: .large)
-            .navigationBarItems(
-                leading: HStack {
-                    LeadingButtonSFSymbol("text.badge.star") {
-                        self.showFavorites = true
-                    }
-                    
-                    RegionPicker(selectedRegion: self.$store.selectedRegion)
-                },
-                trailing: HStack {
-                    TrailingFavoriteToggleButton(region: $store.selectedRegion)
-                    
-                    TrailingButtonSFSymbol("text.badge.star") {
-                        self.showFavorites = true
-                    }
-                    
-                    TrailingButtonSFSymbol("arrow.2.circlepath") {
-                        self.store.fetch()
-                        self.territories.fetch()
-                    }
-            })
-                .sheet(isPresented: $showFavorites) {
-                    RegionsView(selected: self.$store.selectedRegion)
-                        .environmentObject(self.store)
-                        .environmentObject(self.territories)
-            }
         }
+        .padding(.horizontal)
     }
 }
 
 struct SimpleChartView_Previews: PreviewProvider {
     static var previews: some View {
-        SimpleChartView()
-            .environmentObject(Store())
-            .environmentObject(Territories())
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            SimpleChartView()
+        }
+        .environmentObject(Store())
+        .environmentObject(Territories())
+        .environment(\.colorScheme, .dark)
     }
 }
